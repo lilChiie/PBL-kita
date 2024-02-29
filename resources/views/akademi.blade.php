52074a42d4cdb58948a1025086b4bb4747c7face
Niati <niatiniati545@gmail.com>
2024-02-29 14:56:16 +0700

    akademi v2

--- resources/views/akademi.blade.php
+++ resources/views/akademi.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ini halaman akademi 
+</body>
+</html>
